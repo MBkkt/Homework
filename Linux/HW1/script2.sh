1e2d5ab24081aa50a5a224a00fc36d73a1e2be80
MBkkt <32071355+MBkkt@users.noreply.github.com>
2018-10-16 01:09:27 +0300

    Create script2.sh

---
+++ Linux/HW1/script2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ./script2.sh [-d] replace [mask] directory
+while [[ -n $1 ]]; do
+  case $1 in
+    --) shift ;;
+    -d) key="d"; shift;;
+    *) break;;
+  esac
+done
+
+sfx=$1
+shift
+
+argv="$@"
+
+t=${!#}
+if [[ ! -d $t ]]; then t=`pwd`; fi
+cd $t
+
+for x in $argv; do
+  if [[ -f $x ]]; then
+    a=${x%.*}
+    b=${x#${a}}
+    y=${a}${sfx}${b}
+    echo "$x -> $y"
+    if [[ -z $key ]]; then mv "$x" "$y"; fi
+  fi
+done
