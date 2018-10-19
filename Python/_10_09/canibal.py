775567d929d1795ac7e3ad099b0d772f719feb82
MBkkt <32071355+MBkkt@users.noreply.github.com>
2018-10-19 21:25:58 +0300

    Update canibal.py

--- Python/_10_09/canibal.py
+++ Python/_10_09/canibal.py
@@ -5,7 +5,6 @@
 если на каком-нибудь из берегов каннибалов окажется больше, чем миссионеров,
 то миссионеры будут съедены. Найдите такую последовательность перевозок,
 чтобы все безопасно оказались на другом берегу реки."""
-from itertools import product
 
 
 def solve(m_count, c_count, boat=1):
@@ -14,11 +13,11 @@ def solve(m_count, c_count, boat=1):
     elif boat > 0:
         if m_count < c_count:
             print(f'2 cannibals go                    '
-                  f'{m_count} missionaries and {c_count-2} cannibals left')
+                  f'{m_count} missionars and {c_count-2} cannibals left')
             solve(m_count, c_count - 2, boat - 1)
         else:
-            print(f'1 missionary and 1 cannibal go    '
-                  f'{m_count-1} missionaries and {c_count-1} cannibals left')
+            print(f'1 missionar and 1 cannibal go    '
+                  f'{m_count-1} missionars and {c_count-1} cannibals left')
             solve(m_count - 1, c_count - 1, boat - 1)
     else:
         print('One cannibal back')
