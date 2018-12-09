ee8fd160c010c6668863cd1b784926152a1e594a
MBkkt <32071355+MBkkt@users.noreply.github.com>
2018-12-09 15:30:09 +0300

    Update app.py

--- Python/_11_13/app.py
+++ Python/_11_13/app.py
@@ -2,6 +2,6 @@ from _11_06.tools import Factory
 
 if __name__ == '__main__':
     try:
-        F = Factory(load=True, count=100, debug=True)
+        F = Factory()
     except Exception as e:
         print(e)
