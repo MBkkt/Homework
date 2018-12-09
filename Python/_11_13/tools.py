c475354e155f6d3100659d744b3d7f8895aef19b
MBkkt <32071355+MBkkt@users.noreply.github.com>
2018-12-09 14:29:51 +0200

    Add files via upload

--- Python/_11_13/tools.py
+++ Python/_11_13/tools.py
@@ -1,6 +1,6 @@
 import json
-import os
 from difflib import SequenceMatcher
+from os.path import isfile
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,28 +8,18 @@ from bs4 import BeautifulSoup
 
 ########################################################################################################################
 
-def _text_transform(s):
-    return s.get_text().strip()
-
-
-########################################################################################################################
-
 class MyException(Exception):
-    def __init__(self):
-        pass
+    pass
 
 
 class NoHaveInternet(MyException):
-    def __init__(self):
-        pass
-
     def __str__(self):
         return (f'Ошибка: Не обнаружено интернет соединение, попытайтесь восстановить его\n'
                 f'Чтобы попытаться еще раз[L], завершить работу[E]')
 
 
 class NoHaveSite(MyException):
-    def __init__(self, count):
+    def __init__(self, count=0):
         self.count = count
 
     def __str__(self):
@@ -37,19 +27,16 @@ class NoHaveSite(MyException):
                 f'Хотите продолжить[C] или попытаться загурзить нужные сайты еще раз[L]')
 
 
-class NoHaveSiteDescribe(MyException):
-    def __init__(self, count):
+class BadParsing(MyException):
+    def __init__(self, count=0):
         self.count = count
 
     def __str__(self):
-        return (f'Ошибка: {self.count} у стольких сайтов не нашлось описания. '
+        return (f'Ошибка: {self.count} столько стайтов парсер не смог обработать'
                 f'Хотите продолжить[C] или попытаться загурзить сайты с нужным описанием еще раз[L]')
 
 
 class FileNoExist(MyException):
-    def __init__(self):
-        pass
-
     def __str__(self):
         return (f'Ошибка: Файла для записи с таким именем не существует. '
                 f'Чтобы ввести верное название файла[F], создать файл для записи с нужным именем[N], '
@@ -71,75 +58,89 @@ class Factory:
         self._run()
 
     def _run(self):
-        try:
-            if self._load:
-                self._jokes()
-                self._load = False
-            if self._app:
-                self._news()
-                self._app = False
-        except MyException as e:
-            print(e)
-            flag = input()
-            if flag == 'L':
-                self._run()
-            if flag in ('F', 'N'):
-                self.jokes_json_file = input()
-                self.jokes_file = input()
-                self.result_file = input()
-                if flag == 'N':
-                    f1 = open(self.jokes_json_file, 'a')
-                    f2 = open(self.jokes_file, 'a')
-                    f3 = open(self.result_file, 'a')
-                    f1.close()
-                    f2.close()
-                    f3.close()
-                self._run()
+        while self._load and self._app:
+            try:
+                try:
+                    requests.get(r'https://www.google.com/')
+                except Exception:
+                    raise NoHaveInternet
+                if self._load:
+                    self._jokes()
+                    self._load = False
+                if self._app:
+                    self._news()
+                    self._app = False
+            except MyException as e:
+                print(e)
+                flag = input()
+                if flag == 'L':
+                    continue
+                if flag in ('F', 'N'):
+                    if not isfile(self.jokes_file):
+                        self.jokes_file = input('Имя файла с текстом шуток: ')
+                    if not isfile(self.jokes_json_file):
+                        self.jokes_json_file = input('Имя файла с json шутками: ')
+                    if not isfile(self.result_file):
+                        self.result_file = input('Имя файла с результатом работы приложения: ')
+                    if flag == 'N':
+                        f1 = open(self.jokes_json_file, 'a')
+                        f2 = open(self.jokes_file, 'a')
+                        f3 = open(self.result_file, 'a')
+                        f1.close()
+                        f2.close()
+                        f3.close()
 
     def _jokes(self):
         error1 = error2 = 0
         num = 453082
-        for num in range(num, num - self._write_count, -1):
-            url = f'https://bash.im/quote/{num}'
-            try:
-                joke_ = Joke(url, self._debug)
-                if joke_.descr:
-                    joke_.save(self.jokes_file)
-                    joke_.to_JSON(self.jokes_json_file)
-                else:
-                    raise NoHaveSiteDescribe
-            except NoHaveSiteDescribe:
-                error1 += 1
-                continue
-            except ConnectionError:
-                error2 += 1
-                continue
-        if error2 == self._write_count:
-            raise NoHaveInternet()
-        if error1 != 0:
-            raise NoHaveSiteDescribe(error1)
+        try:
+            for num in range(num, num - self._write_count, -1):
+                url = f'https://bash.im/quote/{num}'
+                try:
+                    joke_ = Joke(url, self._debug)
+                    if joke_.descr:
+                        joke_.save(self.jokes_file)
+                        joke_.to_json(self.jokes_json_file)
+                    else:
+                        raise BadParsing
+                except BadParsing:
+                    error1 += 1
+                    continue
+                except NoHaveSite:
+                    error2 += 1
+                    continue
+        except Exception:
+            if error2 != 0:
+                raise NoHaveSite(error2)
+            if error1 != 0:
+                raise BadParsing(error1)
         if error2 != 0:
             raise NoHaveSite(error2)
+        if error1 != 0:
+            raise BadParsing(error1)
 
     def _news(self):
-        counter = error = 0
+        error1 = error2 = 0
         try:
             news_sites = self._parse_rss()
         except Exception:
-            raise NoHaveInternet()
+            raise NoHaveSite
         for url in news_sites:
             try:
-                counter += 1
                 new_ = New(url, self._debug)
                 if new_.name and new_.p1 and new_.descr:
                     new_.find_(self.jokes_file)
                     new_.save(self.result_file)
-            except ConnectionError:
-                error += 1
-        if counter == error != 0:
-            raise NoHaveInternet()
-        if error != 0:
-            raise NoHaveSite(error)
+            except BadParsing:
+                error1 += 1
+                continue
+            except NoHaveSite:
+                error2 += 1
+                continue
+        if error2 != 0:
+            raise NoHaveSite(error2)
+        if error1 != 0:
+            raise BadParsing(error1)
 
     def _parse_rss(self):
         answer = []
@@ -147,7 +148,7 @@ class Factory:
         html = html.replace('windows-1251', 'utf-8', 1)
         soup = BeautifulSoup(html, features='lxml-xml')
         for x in soup.find_all('link'):
-            x = _text_transform(x)
+            x = x.get_text().strip()
             if x not in (r'https://www.rbc.ru/', '',):
                 answer.append(x)
         return answer
@@ -158,19 +159,22 @@ class Factory:
 class Joke:
     def __init__(self, url, debug):
         self._debug = debug
-        self._html = requests.get(url).text
-        self._soup = BeautifulSoup(self._html, features='lxml')
-        self._get_description()
+        try:
+            self._html = requests.get(url).text
+        except Exception:
+            raise NoHaveSite
+        try:
+            self._soup = BeautifulSoup(self._html, features='lxml')
+            self._get_description()
+        except Exception:
+            raise BadParsing
 
     def _get_description(self):
-        try:
-            x = self._soup.find('div', {'class': 'text'})
-            self.descr = _text_transform(x)
-        except Exception:
-            raise NoHaveSiteDescribe
+        x = self._soup.find('div', {'class': 'text'})
+        self.descr = x.get_text().strip()
 
     def save(self, file_):
-        if not os.path.isfile(file_):
+        if not isfile(file_):
             raise FileNoExist
         with open(file_, mode='a', encoding='utf-8') as f:
             if not self._debug:
@@ -178,13 +182,14 @@ class Joke:
             else:
                 print(self.descr)
 
-    def _default(self, instance):
+    @staticmethod
+    def _default(instance):
         return {k: v
                 for k, v in vars(instance).items()
                 if not str(k).startswith('_')}
 
-    def to_JSON(self, file_):
-        if not os.path.isfile(file_):
+    def to_json(self, file_):
+        if not isfile(file_):
             raise FileNoExist
         with open(file_, mode='a', encoding='utf-8') as f:
             x = json.dumps(self, default=self._default)
@@ -201,33 +206,33 @@ class New:
     def __init__(self, url, debug):
         self._debug = debug
         self.url = url
-        self._html = requests.get(url).text
-        self._soup = BeautifulSoup(self._html, features='lxml')
-        self._get_name()
-        self._get_description_and_p1()
+        self.simular_joke = ''
+        try:
+            self._html = requests.get(url).text
+        except Exception:
+            raise NoHaveSite
+        try:
+            self._soup = BeautifulSoup(self._html, features='lxml')
+            self._get_name()
+            self._get_description_and_p1()
+        except:
+            raise BadParsing
 
     def _get_name(self):
-        try:
-            x = self._soup.find('div', {'class': 'article__header__title'})
-            self.name = _text_transform(x)
-        except Exception as e:
-            raise NoHaveSiteDescribe
+        x = self._soup.find('div', {'class': 'article__header__title'})
+        self.name = x.get_text().strip()
 
     def _get_description_and_p1(self):
         self.descr = ''
         self.p1 = ''
-        try:
-            for k, x in enumerate(self._soup.find('div', {'class': 'article__text'}).find_all('p'), start=1):
-                if k == 1:
-                    self.p1 = _text_transform(x)
-                self.descr += _text_transform(x)
-        except Exception:
-            raise NoHaveSiteDescribe
+        for k, x in enumerate(self._soup.find('div', {'class': 'article__text'}).find_all('p'), start=1):
+            if k == 1:
+                self.p1 = x.get_text().strip()
+            self.descr += x.get_text().strip()
 
     def find_(self, file_, limit=1000):
-        if not os.path.isfile(file_):
+        if not isfile(file_):
             raise FileNoExist
-        self.simular_joke = ''
         comp = SequenceMatcher(a=self.descr, b=self.descr)
         mx = 0
         with open(file_, mode='r', encoding='utf-8') as f:
@@ -241,7 +246,7 @@ class New:
                     break
 
     def save(self, file_):
-        if not os.path.isfile(file_):
+        if not isfile(file_):
             raise FileNoExist
         with open(file_, 'a', encoding='utf-8') as f:
             s = (f'Заголовок новости: {self.name}\n'
