9dca15d20daa78ce8a6321a20714f0ba17e3904e
MBkkt <valera.mironow@gmail.com>
2019-09-13 08:05:31 +0300

    task 1 upgrade

--- Algorithm_2/Homework_1/task_1.cpp
+++ Algorithm_2/Homework_1/task_1.cpp
@@ -4,10 +4,11 @@
 #include <string_view>
 #include <vector>
 
-std::vector<size_t> z_function(const std::string_view s) {
-    size_t n = s.length();
+size_t find_new_substr(std::string s, size_t pos) {
+    std::reverse(s.begin(), s.end());
+    size_t n = pos, max = 0;
     std::vector<size_t> z(n, 0);
-    for (size_t i = 1, l = 0, r = 0; i < n; ++i) {
+    for (size_t i = 1, l = 0, r = 0; i < pos; ++i) {
         if (i <= r) {
             z[i] = std::min(r - i + 1, z[i - l]);
         }
@@ -18,22 +19,19 @@ std::vector<size_t> z_function(const std::string_view s) {
             l = i;
             r = i + z[i] - 1;
         }
+        if (z[i] > max) {
+            max = z[i];
+        }
     }
-    return z;
-}
-
-size_t find_max_z_func(std::string s) {
-    std::reverse(s.begin(), s.end());
-    auto temp = z_function(s);
-    return *std::max_element(temp.begin(), temp.end());
+    return pos - max;
 }
 
 int main() {
     size_t ans = 0;
     std::string s;
     std::cin >> s;
-    for (size_t i = 0; i != s.size(); ++i) {
-        ans += (i + 1 - find_max_z_func(s.substr(0, i + 1)));
+    for (size_t i = 0; i < s.size(); ++i) {
+        ans += find_new_substr(s, i + 1);
     }
     std::cout << ans;
     return 0;
